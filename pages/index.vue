384772b74409d1cde341f8921e6a3d91c43f1e83
Emilio P <ea.pugaavalos.99@gmail.com>
2023-05-18 22:07:16 -0600

    Remove template around artcard and move QDialogs out of div

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,41 @@
 <template>
   <div class="tw-relative tw-flex tw-justify-center">
     <div class="tw-columns-auto tw-gap-4 sm:tw-columns-2 md:tw-columns-3 lg:tw-columns-4 xl:tw-columns-5">
-      <template
+      <ArtCard
         v-for="(sub, i) in test"
         :key="i"
-      >
-        <ArtCard
-          class="tw-mb-4 tw-break-inside-avoid"
-          :submission="sub"
-          @open-image-viewer="openImageDialog"
-          @open-model-viewer="openModelDialog"
-        />
-      </template>
-      <QDialog
-        v-model="showImageDialog"
-        maximized
-        @click="showImageDialog = false"
-      >
-        <div class="2d-viewer tw-flex tw-w-full tw-flex-row tw-items-center tw-justify-center">
-          <img
-            class="tw-h-[85vh] tw-w-auto tw-rounded-md tw-bg-white"
-            :src="imageUrl"
-          >
-        </div>
-      </QDialog>
-      <QDialog
-        v-model="showModelDialog"
-        maximized
-        @click="showModelDialog = false"
-      >
-        <div class="3d-viewer tw-flex tw-w-full tw-flex-row tw-items-center tw-justify-center">
-          <div class="tw-overflow-clip tw-rounded-md tw-bg-white">
-            <ModelViewer
-              :model="modelUrl"
-              class="tw-aspect-square tw-h-[85vh]"
-            />
-          </div>
-        </div>
-      </QDialog>
+        class="tw-mb-4 tw-break-inside-avoid"
+        :submission="sub"
+        @open-image-viewer="openImageDialog"
+        @open-model-viewer="openModelDialog"
+      />
     </div>
+    <QDialog
+      v-model="showImageDialog"
+      maximized
+      @click="showImageDialog = false"
+    >
+      <div class="2d-viewer tw-flex tw-w-full tw-flex-row tw-items-center tw-justify-center">
+        <img
+          class="tw-h-[85vh] tw-w-auto tw-rounded-md tw-bg-white"
+          :src="imageUrl"
+        >
+      </div>
+    </QDialog>
+    <QDialog
+      v-model="showModelDialog"
+      maximized
+      @click="showModelDialog = false"
+    >
+      <div class="3d-viewer tw-flex tw-w-full tw-flex-row tw-items-center tw-justify-center">
+        <div class="tw-overflow-clip tw-rounded-md tw-bg-white">
+          <ModelViewer
+            :model="modelUrl"
+            class="tw-aspect-square tw-h-[85vh]"
+          />
+        </div>
+      </div>
+    </QDialog>
   </div>
 </template>
 
